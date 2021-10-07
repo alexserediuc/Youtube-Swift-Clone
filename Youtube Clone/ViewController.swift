e4f06c53affda95b02a0e210b338ae7cf126a3cd
Alex <alexserediuc99@gmail.com>
2021-10-07 16:47:19 +0300

    basic main design

--- Youtube Clone/ViewController.swift
+++ Youtube Clone/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tabBarView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tabBarView.layer.borderWidth = 1
+        tabBarView.layer.borderColor = UIColor.lightGray.cgColor
     }
 
 
